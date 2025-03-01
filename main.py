de3894430b4988031536a748cd803fa06904bb8c
Milos116 <milos.mitic@hotmail.com>
2025-03-02 00:07:41 +0100

    Finished

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from stats import word_count
+from stats import word_count,character_count, sort_char_count
+import sys
 
 def get_book_text(file_path):
     with open (file_path, encoding='utf-8') as f:
         file_contents = f.read()
     return file_contents
 
+
 def main():
-    word_count(get_book_text("books/frankenstein.txt"))
+
+    args = sys.argv
+
+    if len(args) < 2:
+        print ("Requiers path of the book you wish to be analyzed (as argument)")
+        sys.exit(1)
+
+    file_path = args[1]
+
+    content = get_book_text(file_path)
+
+    chars = character_count(content)
+
+    sorted_chars = sort_char_count(chars)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt")
+    print("----------- Word Count ----------")
+    word_count(content)
+    print("--------- Character Count -------")
+    for sorted in sorted_chars:
+        if sorted["char"].isalpha():
+            print (f"{sorted["char"]}:{sorted["count"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,30 @@
 def word_count(content):
     word_list= content.split()
     num_word = len(word_list)
-    print(f"{num_word} words found in the document")
+    print(f"Found {num_word} total words")
+
+
+
+def character_count(content):
+    char_dict = {}
+    for char in content:
+        if char.lower() in char_dict:
+            char_dict[char.lower()] += 1
+        else:
+            char_dict[char.lower()] = 1
+    return char_dict
+
+def sort_char_count(char_dict):
+
+    chars_list = []
+
+    for char, count in char_dict.items():
+        char_dict = {"char": char, "count": count}
+        chars_list.append(char_dict)
+
+    def sort_on(dict):
+        return dict["count"]
+    
+    chars_list.sort(reverse=True, key=sort_on)
+
+    return chars_list
